812041a6df232f1a3eeebb12affbeaf485a4942a
Jamy Golden <code@jamygolden.com>
2024-10-23 18:20:55 +0200

    Make highlighted ansi text white

--- ansi/ansi.sh
+++ ansi/ansi.sh
@@ -4,6 +4,6 @@
 
 export FZF_DEFAULT_OPTS=" \
  --color=bg:0,fg:7,hl:3\
- --color=bg+:8,fg+:0,hl+:11\
+ --color=bg+:8,fg+:7,hl+:11\
  --color=info:3,border:3,prompt:4\
  --color=pointer:0,marker:9,spinner:9,header:1"
